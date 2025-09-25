98bdc5e1ced00c76210703c0754aa51b4f6009d7
Stefano Fornari <stefano.fornari@gmail.com>
2025-09-25 12:17:30 +0200

    headless testing

--- src/test/java/come/example/SwingAppTest.java
+++ src/test/java/come/example/SwingAppTest.java
@@ -1,11 +1,13 @@
 package come.example;
 
 import com.example.SwingApp;
+import com.github.caciocavallosilano.cacio.ctc.junit.CacioTest;
 import org.junit.jupiter.api.Test;
 
 /**
  *
  */
+@CacioTest
 public class SwingAppTest {
     @Test
     public void test() {
